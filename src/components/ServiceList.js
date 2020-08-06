fc46d6ffbe255643b8ef9b20dc407cbd34f3421c
Vlad <vladlisin2@gmail.com>
2020-08-06 18:07:32 +0700

    home

--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import Background from '../static/background.png';
+import Background from '../static/background.jpg';
 import { Link } from 'react-router-dom';
 
 
